e3ccd6f56dcf614991be69ce67fc36fd771e592d
Fasty <124357939+Fasty-Trinita@users.noreply.github.com>
2025-05-19 10:00:02 +0530

    Add files via upload

---
+++ day2hw/LibraryManagement.java
@@ -0,0 +1,14 @@
+package week1.day2hw;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Library lib = new Library();
+		String libone = lib.bookTitle();
+		System.out.println("Book Title: " + libone);
+		lib.issueBook();
+
+	}
+
+}
